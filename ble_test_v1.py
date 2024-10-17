89df8fb013813089a1c24ab29e263d4a1d964087
radicdjolee <114096761+radicdjolee@users.noreply.github.com>
2024-10-17 18:42:20 +0000

    v19

--- ble_test_v1.py
+++ ble_test_v1.py
@@ -1,99 +1,35 @@
-"""
-Example for a BLE 4.0 Server using a GATT dictionary of services and
-characteristics
-"""
-import sys
-import logging
-import asyncio
-import threading
+from bluepy import btle
 
-from typing import Any, Dict, Union
+# Definiši UUID za uslugu
+UUID_SERVICE = "12345678-1234-5678-1234-56789abcdef0"
 
-from bless import (  # type: ignore
-    BlessServer,
-    BlessGATTCharacteristic,
-    GATTCharacteristicProperties,
-    GATTAttributePermissions,
-)
+class MyBLEServer(btle.Peripheral):
+    def __init__(self):
+        super().__init__()
+        
+        # Dodaj uslugu
+        self.service = btle.Service(UUID_SERVICE)
+        self.addService(self.service)
 
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(name=__name__)
+    def advertise(self):
+        self.setAdvertisingData(b'\x02\x01\x06' + self.service.getHandle())
+        self.advertiseStart()
 
-trigger: Union[asyncio.Event, threading.Event]
-if sys.platform in ["darwin", "win32"]:
-    trigger = threading.Event()
-else:
-    trigger = asyncio.Event()
+def main():
+    server = MyBLEServer()
 
+    # Oglasi se
+    server.advertise()
+    print("BLE server je sada vidljiv.")
 
-def read_request(characteristic: BlessGATTCharacteristic, **kwargs) -> bytearray:
-    logger.debug(f"Reading {characteristic.value}")
-    return characteristic.value
+    # Održavaj program u radu
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        print("Zaustavljam server...")
+    finally:
+        server.disconnect()
 
-
-def write_request(characteristic: BlessGATTCharacteristic, value: Any, **kwargs):
-    characteristic.value = value
-    logger.debug(f"Char value set to {characteristic.value}")
-    if characteristic.value == b"\x0f":
-        logger.debug("Nice")
-        trigger.set()
-
-
-async def run(loop):
-    trigger.clear()
-
-    # Instantiate the server
-    gatt: Dict = {
-        "A07498CA-AD5B-474E-940D-16F1FBE7E8CD": {
-            "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B": {
-                "Properties": (
-                    GATTCharacteristicProperties.read
-                    | GATTCharacteristicProperties.write
-                    | GATTCharacteristicProperties.indicate
-                ),
-                "Permissions": (
-                    GATTAttributePermissions.readable
-                    | GATTAttributePermissions.writeable
-                ),
-                "Value": None,
-            }
-        },
-        "5c339364-c7be-4f23-b666-a8ff73a6a86a": {
-            "bfc0c92f-317d-4ba9-976b-cc11ce77b4ca": {
-                "Properties": GATTCharacteristicProperties.read,
-                "Permissions": GATTAttributePermissions.readable,
-                "Value": bytearray(b"\x69"),
-            }
-        },
-    }
-    my_service_name = "Test Service"
-    server = BlessServer(name=my_service_name, loop=loop)
-    server.read_request_func = read_request
-    server.write_request_func = write_request
-
-    await server.add_gatt(gatt)
-    await server.start()
-    logger.debug(server.get_characteristic("51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B"))
-    logger.debug("Advertising")
-    logger.info(
-        "Write '0xF' to the advertised characteristic: "
-        + "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B"
-    )
-    if trigger.__module__ == "threading":
-        trigger.wait()
-    else:
-        await trigger.wait()
-    await asyncio.sleep(2)
-    logger.debug("Updating")
-    server.get_characteristic("51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B").value = bytearray(
-        b"i"
-    )
-    server.update_value(
-        "A07498CA-AD5B-474E-940D-16F1FBE7E8CD", "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B"
-    )
-    await asyncio.sleep(5)
-    await server.stop()
-
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(run(loop))
+if __name__ == "__main__":
+    main()
